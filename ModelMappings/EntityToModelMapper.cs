3938498eb2d209add251f1936d1c3c37ad72f8aa
guyi34super <67825986+guyi34super@users.noreply.github.com>
2022-09-28 21:06:54 +0400

    Add files via upload

---
+++ ModelMappings/EntityToModelMapper.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Elca.Sms.Api.Domain.Entity;
+using ELCAStock.Models;
+
+namespace ELCAStock.ModelMappings
+{
+    public class EntityToModelMapper : Profile
+    {
+        public EntityToModelMapper()
+        {
+            CreateMap<ItemRequest, ItemRequestDTO>();
+            CreateMap<OrderBatch, OrderBatchDTO>();
+            CreateMap<OrderBatchSupplier, OrderBatchSupplierDTO>();
+            CreateMap<Order, OrderDTO>();
+            CreateMap<OrderItem, OrderItemDTO>();
+            CreateMap<Product, ProductDTO>();
+            CreateMap<ProductLine, ProductLineDTO>();
+            CreateMap<ProductItem, ProductItemDTO>();
+            CreateMap<ProductType, ProductTypeDTO>();
+            CreateMap<Requestor, RequestorDTO>();
+            CreateMap<Supplier, SupplierDTO>();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ModelMappings/ModelToEntityMapper.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Elca.Sms.Api.Domain.Entity;
+using ELCAStock.Models;
+
+namespace ELCAStock.ModelMappings
+{
+    public class ModelToEntityMapper : Profile
+    {
+        public ModelToEntityMapper()
+        {
+            CreateMap<ItemRequestDTO, ItemRequest>();
+            CreateMap<OrderBatchDTO, OrderBatch>();
+            CreateMap<OrderBatchSupplierDTO, OrderBatchSupplier>();
+            CreateMap<OrderDTO, Order>();
+            CreateMap<OrderItemDTO, OrderItem>();
+            CreateMap<ProductDTO, Product>();
+            CreateMap<ProductLineDTO, ProductLine>();
+            CreateMap<ProductItemDTO, ProductItem>();
+            CreateMap<ProductTypeDTO, ProductType>();
+            CreateMap<RequestorDTO, Requestor>();
+            CreateMap<SupplierDTO, Supplier>();
+
+
+        }
+    }
+}
